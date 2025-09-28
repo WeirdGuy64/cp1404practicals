a126c16bc645167a03b341f9896fd6d52836e84e
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-09-28 21:10:39 +1000

    Refactor score program to use functions and add part at the bottom of main function to generate random score and print the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,25 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+from random import randint
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    print(return_result(score))
+    random_score = randint(0, 100)
+    print(return_result(random_score))
+
+
+def return_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
