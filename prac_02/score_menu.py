ae5847983379bd107697aefe85a324d44c65c1b9
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-09-28 22:02:28 +1000

    Refactor score menu program to use functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -5,36 +5,46 @@ Q - Quit"""
 
 
 def main():
-    score = int(input("Score (0-100): "))
-    while score < 0 or score > 100:
-        print("Invalid score")
-        score = int(input("Score (0-100): "))
+    score = get_score()
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = int(input("Score (0-100): "))
-            while score < 0 or score > 100:
-                print("Invalid score")
-                score = int(input("Score (0-100): "))
+            score = get_score()
         elif choice == "P":
-            if score < 0 or score > 100:
-                print("Invalid score")
-            elif score >= 90:
-                print("Excellent")
-            elif score >= 50:
-                print("Passable")
-            else:
-                print("Bad")
+            print(return_result(score))
         elif choice == "S":
-            for character in range(score):
-                print("*", end="")
+            print_stars(score)
             print()
         else:
             print("That is not an option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Farewell")
+
+
+def print_stars(score):
+    for character in range(score):
+        print("*", end="")
+
+
+def return_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_score():
+    score = int(input("Score (0-100): "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Score (0-100): "))
+    return score
 
 
 main()
