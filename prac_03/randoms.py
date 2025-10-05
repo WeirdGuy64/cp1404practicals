976f457dc5cbe68d4cc693ac365bd3e26cca5b09
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-10-05 22:07:17 +1000

    Answer first three questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+# On line 1, I saw the number 8. The smallest the number that could have been seen is 5. The largest number that could have been seen is 20.
+
+# On line 2, I saw the number 7. The smallest number that could have been seen is 3. The largest number that could have been seen is 9. No, line 2 could not have produced a 4.
+
+# On line 3, I saw the number 3.8154197638486704. The smallest number that could have been seen is 2.5. The largest number that could have been seen is 5.5.
