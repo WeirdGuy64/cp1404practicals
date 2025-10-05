7676f43d13c19c654ab2bd82245be43e532a7d93
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-10-06 00:21:35 +1000

    Add code that opens name.txt and reads the name and prints a message with that name

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,8 @@ name = input("Name: ")
 out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
+
+# 2.
+in_file = open("name.txt", "r")
+print(f"Hi {in_file.read(len(name))}!")
+in_file.close()
