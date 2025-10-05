ae5c01ddbc71136c43958ff2019191f6cf1808a9
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-10-05 23:24:44 +1000

    Answer all 3 questions

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -4,6 +4,11 @@ Answer the following questions:
 1. When will a ValueError occur?
 2. When will a ZeroDivisionError occur?
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+
+Answers:
+1. When the numerator or the denominator inputs are not integers
+2. When the denominator input is 0
+3. You could add an if statement that detects if the user inputs 0 and stop the calculation if an input of 0 is detected, but I think it would quicker and easier to just use exceptions
 """
 
 try:
