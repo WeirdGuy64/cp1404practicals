60b74eebf9e97703cf4952ab59800e64cda15b7a
WeirdGuy64 <timothy.chappell@my.jcu.edu.au>
2025-10-05 22:10:12 +1000

    Add code that produces a random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,7 @@
 # On line 2, I saw the number 7. The smallest number that could have been seen is 3. The largest number that could have been seen is 9. No, line 2 could not have produced a 4.
 
 # On line 3, I saw the number 3.8154197638486704. The smallest number that could have been seen is 2.5. The largest number that could have been seen is 5.5.
+
+import random
+
+print(random.randint(1, 100))
